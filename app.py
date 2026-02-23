6a54543b0e6527037f9d0187f141990216c5502b
Josphat <gichimujosphat362@gmail.com>
2026-02-23 13:36:51 +0300

    added getproducts

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask  import *
 import pymysql
+import os
+
 app=Flask(__name__)
+app.config["UPLOAD_FOLDER"]="static/images"
 
 @app.route("/api/signup",methods=["POST"])
 def signup():
@@ -50,7 +53,52 @@ def signin():
         user=cursor.fetchone()
         return jsonify({"message":"log in successful","user":user})
 
+@app.route("/api/add_product",methods=["POST"])
+def addproduct():
+    product_name = request.form["product_name"]
+    product_description= request.form["product_description"]
+    product_category= request.form["product_category"]
+    product_cost= request.form["product_cost"]
+    product_image=request.files["product_image"]
 
+
+    print(product_name,product_description,product_category,product_cost,product_image)
+    # get image name
+    image_name=product_image.filename
+    print(image_name)
+
+    # save images to the images folder
+    file_path=os.path.join(app.config["UPLOAD_FOLDER"], image_name)
+    print(file_path)
+    product_image.save(file_path)
+
+    # create a connection to the database
+    connection=pymysql.connect(host="localhost",user="root",password="",database="josphat_sokogarden")
+
+    cursor= connection.cursor()
+
+    sql="insert into product_details(product_name, product_description, product_category, product_cost, product_image) values (%s,%s,%s,%s,%s)"
+    # data to execute
+    data=(product_name,product_description,product_category,product_cost, image_name)
+    # execute query
+    cursor.execute(sql,data)
+    # save the data
+    connection.commit()
+
+    return jsonify({"message":"product added successfully"})
+
+@app.route("/api/get_products")    
+def getproducts():
+    connection=pymysql.connect(host="localhost",user="root",password="",database="josphat_sokogarden")
+    cursor=connection.cursor(pymysql.cursors.DictCursor)
+    sql="select * from product_details"
+    cursor.execute(sql)
+    if cursor.rowcount==0:
+        return jsonify({"message":"Out of stock"})
+    else:
+        # fetch products
+        products= cursor.fetchall()
+        return jsonify(products)
 
 
 
